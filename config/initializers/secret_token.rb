040d82c97e56d0881c477727a956ea1d584e5e21
Andrew <anadrew@getsmarter.co.za>
2014-01-09 11:36:34 +0200

    Add a StaticPages controler

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,19 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-#SampleApp::Application.config.secret_key_base = '7518854f555b40cffe7f0ecc000852f54976c78fca905494b97c3ddc2112c6c417380cbfea65dd7261feaef6431c54751f3ea15f0f82ab1fe9118b33cdd3acf8'
 require 'securerandom'
 
 def secure_token
-	token_file=Rails.root.join('.secret')
-	if File.exist?(token_file)
-		#use the existing token
-		File.read(token_file).chop
-	else
-		#generate a new token and store it in the token file
-		token=securerandom.hex(64)
-		File.write(token_file,token)
-		token
-	end
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
 end
+
 SampleApp::Application.config.secret_key_base = secure_token
